2c42a9ea8112f39aa4bf0ab84a4424eb59ea5a91
Adrien Dengreville <didien@users.noreply.github.com>
2017-10-09 14:29:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,7 +40,12 @@ install xf86-input-synaptics
 show "\tAwesome"
 install awesome
 mkdir -p ~/.config/awesome/
-fetch_r https://github.com/lcpz/awesome-copycats.git
+fetch https://github.com/lcpz/awesome-copycats.git
+cd awesome-copycats
+rm -rf lain freedesktop
+fetch https://github.com/lcpz/awesome-freedesktop.git freedesktop
+fetch https://github.com/lcpz/lain.git
+cd ..
 mv -bv awesome-copycats/* ~/.config/awesome
 rm -rf awesome-copycats
 
